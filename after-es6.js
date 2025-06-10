b8f963b18ff53cc40dec510769995a27ca3f3100
Nur Khalida <156413056+nurkhalida29@users.noreply.github.com>
2025-06-10 10:09:50 +0700

    Create after-es6.js

---
+++ after-es6.js
@@ -0,0 +1,16 @@
+<script>
+  // file: after-es6.js
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+const person2 = new Person('KHALIDA');
+person2.sayHello();
+
+  <script>
